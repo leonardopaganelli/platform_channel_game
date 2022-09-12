9ad29cdbea3f23a8bb57228ee6a53178654d7626
Leonardo Paganelli <leonardo.paganelli@outlook.com>
2022-09-12 20:30:42 -0300

    feat: initial commit with layout

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color colorLightBlue = const Color.fromARGB(255, 65, 105, 216);
+Color colorMediumBlue = const Color.fromARGB(255, 58, 95, 200);
+Color colorDarkBlue = const Color.fromARGB(255, 36, 70, 166);
+Color colorBlueText = const Color.fromARGB(255, 39, 72, 167);
+Color colorOrange = const Color.fromARGB(255, 250, 157, 133);
