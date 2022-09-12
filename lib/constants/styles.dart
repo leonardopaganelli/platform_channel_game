e3f914ccd8d77a630c3d61880be6cfa8f3bee683
Leonardo Paganelli <leonardo.paganelli@outlook.com>
2022-09-12 20:56:08 -0300

    add initial states

---
+++ lib/constants/styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+final textStyle36 = const TextStyle(fontSize: 36, color: Colors.white);
+final textStyle76 = const TextStyle(fontSize: 75, color: Colors.white);
--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -3,6 +3,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:platform_channel_game/constants/colors.dart';
+import 'package:platform_channel_game/constants/styles.dart';
+import 'package:platform_channel_game/models/creator.dart';
 
 class GameWidget extends StatefulWidget {
   const GameWidget({Key? key}) : super(key: key);
@@ -12,6 +14,9 @@ class GameWidget extends StatefulWidget {
 }
 
 class _GameWidgetState extends State<GameWidget> {
+  Creator?  creator;
+  bool myTurn = false;
+
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, designSize: const Size(700, 1400));
@@ -42,10 +47,52 @@ class _GameWidgetState extends State<GameWidget> {
                   color: colorDarkBlue,
                 )
               ],
+            ),
+            Container(
+              height: ScreenUtil().setHeight(1400),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    creator == null
+                      ? Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          buildButton("Criar", true),
+                          SizedBox(width: 10),
+                          buildButton("Entrar", false),
+                        ]
+                      )
+                      : InkWell(
+                        child: Text(
+                          myTurn == true
+                            ? "Faça a sua jogada"
+                            : "Aguarde sua vez",
+                          style: textStyle36,
+                        )
+                      )
+                  ],
+                ),
+              ),
             )
           ],
         ),
       ),
     );
   }
+
+  Widget buildButton(String label, bool owner) => Container(
+    width: ScreenUtil().setHeight(300),
+    child: ElevatedButton(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Text(
+          label,
+          style: textStyle36,
+        ),
+      ),
+      onPressed: () {},
+    ),
+  );
 }
